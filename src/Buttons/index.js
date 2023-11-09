4282c597855e12b1a9a202456d28cea86d330e61
Rafał Olewicz <namerafal75@gmail.com>
2023-11-09 21:43:42 +0100

    task list without interactions yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+const Buttons = ({ tasks, hideDoneTask }) => (
+    <div className="buttons">
+        {tasks.length > 0 && (
+            <>
+                <button
+                    className="buttons__button"
+                    disabled={!tasks.some(({ done }) => done)}
+                >
+                    {hideDoneTask ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button
+                    className="buttons__button"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button>
+            </>
+        )}
+    </div>
+);
+
+export default Buttons;
---
+++ src/Tasks/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css"
+
+const Tasks = ({ tasks, hideDoneTasks }) => (
+	<ul className="tasks">
+		{tasks.map(task => (
+			<li key={task.id} className={`tasks__item ${hideDoneTasks && task.done
+				? "tasks__item--hiden" : ""}`}>
+				<button className="tasks__buttons tasks__buttons--toggleDone">
+					{task.done ? "✔" : " "}
+				</button>
+				<span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+					{task.content}
+				</span>
+				<button className="tasks__buttons tasks__buttons--remove">
+					🗑
+				</button>
+			</li>
+		))}
+	</ul>
+);
+
+
+export default Tasks;
