f8a4b7a2528787d2f0ae8cf6f0de8c7c0e207c93
Rafał Olewicz <namerafal75@gmail.com>
2023-11-13 19:26:02 +0100

    TAB display size, changed to 2 spaces

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,24 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTask }) => (
-    <div className="buttons">
-        {tasks.length > 0 && (
-            <>
-                <button
-                    className="buttons__button"
-                    disabled={!tasks.some(({ done }) => done)}
-                >
-                    {hideDoneTask ? "Pokaż" : "Ukryj"} ukończone
-                </button>
-                <button
-                    className="buttons__button"
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </button>
-            </>
-        )}
-    </div>
+  <div className="buttons">
+    {tasks.length > 0 && (
+      <>
+        <button
+          className="buttons__button"
+          disabled={!tasks.some(({ done }) => done)}
+        >
+          {hideDoneTask ? "Pokaż" : "Ukryj"} ukończone
+        </button>
+        <button
+          className="buttons__button"
+          disabled={tasks.every(({ done }) => done)}
+        >
+          Ukończ wszystkie
+        </button>
+      </>
+    )}
+  </div>
 );
 
 export default Buttons;
