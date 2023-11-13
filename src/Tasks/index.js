d2320344da0a042e30e2d8629dff903c638f11bc
Rafał Olewicz <namerafal75@gmail.com>
2023-11-13 23:35:31 +0100

    TAB display size, changed to 2 spaces

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,22 +1,22 @@
 import "./style.css";
 
 const Tasks = ({ tasks, hideDoneTasks }) => (
-	<ul className="tasks">
-		{tasks.map(task => (
-			<li key={task.id} className={`tasks__item ${hideDoneTasks && task.done
-				? "tasks__item--hiden" : ""}`}>
-				<button className="tasks__buttons tasks__buttons--toggleDone">
-					{task.done ? "✔" : " "}
-				</button>
-				<span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
-					{task.content}
-				</span>
-				<button className="tasks__buttons tasks__buttons--remove">
-					🗑
-				</button>
-			</li>
-		))}
-	</ul>
+  <ul className="tasks">
+    {tasks.map(task => (
+      <li key={task.id} className={`tasks__item ${hideDoneTasks && task.done
+        ? "tasks__item--hiden" : ""}`}>
+        <button className="tasks__buttons tasks__buttons--toggleDone">
+          {task.done ? "✔" : " "}
+        </button>
+        <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+          {task.content}
+        </span>
+        <button className="tasks__buttons tasks__buttons--remove">
+          🗑
+        </button>
+      </li>
+    ))}
+  </ul>
 );
 
 
