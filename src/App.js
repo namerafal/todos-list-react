2acb3f0c454bda79542e096972072539e3b0b83f
Rafał Olewicz <namerafal75@gmail.com>
2024-01-28 12:03:12 +0100

    removed defaultTasks

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-// const defaultTasks = [
-//   { id: 1, content: "przejść na Reacta", done: false },
-//   { id: 2, content: "zjeść śniadanie", done: true },
-// ];
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState([]);
