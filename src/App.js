9f57c27e8fcad01737643487ca68991df4189246
Rafał Olewicz <namerafal75@gmail.com>
2024-05-08 22:58:20 +0200

    useEffect was removed from import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useTasks } from "./useTasks";
 import TaskAddingForm from "./Form";
 import Tasks from "./Tasks";
