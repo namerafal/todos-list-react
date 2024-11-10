81bd57ce0fa8a788364ea0418e40ac6af6e51f47
Rafał Olewicz <namerafal75@gmail.com>
2024-11-10 19:19:46 +0100

    Removed disabled code lines

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,24 +1,11 @@
-// import { useSelector } from "react-redux";
-// import { useTasks } from "../../useTasks";
 import Form from "./Form";
 import TasksList from "./TasksList";
 import Buttons from "./Buttons";
 import AppSection from "../../common/section";
 import Header from "../../common/Header";
 import { MainContainer } from "../../GlobalStyle";
-// import { selectTasks } from "./tasksSlice";
 
 function Tasks() {
-  // const { tasks } = useSelector(selectTasks);
-
-  // const {
-  //   //tasks,
-  //   addNewTask,
-  //   // removeTask,
-  //   // toggleTaskDone,
-  //   allTasksDone,
-  // } = useTasks();
-
   return (
     <MainContainer>
       <Header title={"Lista zadań"} />
@@ -26,20 +13,10 @@ function Tasks() {
         title="Dodaj nowe zadanie"
         body={<Form />}
       />
-
       <AppSection
         title="Lista zadań"
-        extraHeaderContent={
-          <Buttons
-            // allTasksDone={allTasksDone}
-          />
-        }
-        body={
-          <TasksList
-            // removeTask={removeTask}
-            // toggleTaskDone={toggleTaskDone}
-          />
-        }
+        extraHeaderContent={<Buttons />}
+        body={<TasksList />}
       />
     </MainContainer>
   );
