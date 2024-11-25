63b6aac6b7f6849c7af9ac126d30b2fab513196a
Rafał Olewicz <namerafal75@gmail.com>
2024-11-25 21:31:15 +0100

    Added example tasks button

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,16 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import { MainContainer } from "../../GlobalStyle";
 import Header from "../../common/Header";
 import AppSection from "../../common/section";
+import { Button } from "./Buttons/styled";
+import { fetchExampleTasks, selectIsLoading } from "./tasksSlice";
 import Form from "./Form";
 import Buttons from "./Buttons";
 import TasksList from "./TasksList";
 
 function Tasks() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
   return (
     <MainContainer>
       <Header title={"Lista zadań"} />
       <AppSection
         title="Dodaj nowe zadanie"
+        extraHeaderContent={
+          <Button onClick={() => dispatch(fetchExampleTasks())}>
+            {isLoading ? "Pobieram zadania" : "Pobierz przykładowe zadania"}
+          </Button>
+        }
+
         body={<Form />}
       />
       <AppSection
