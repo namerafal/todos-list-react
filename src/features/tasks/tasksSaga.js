264bbff5d55ed3755f5108256a288cb2a308c2af
Rafał Olewicz <namerafal75@gmail.com>
2024-11-25 21:59:41 +0100

    Connecting redux-saga to the store. Locale storage has been restored

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,24 @@
+import { debounce, call, put, takeEvery, select } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+
+    try {        
+        const exampleTasks = yield call(getExampleTasks)
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {    
+    yield debounce(2000, fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+} 
