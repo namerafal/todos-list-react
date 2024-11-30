4b84d0fd4f739e87c372c5f897ed76c838b22982
Rafał Olewicz <namerafal75@gmail.com>
2024-11-30 23:33:50 +0100

    Renamed param name

--- src/features/tasks/TasksPage/TasksList/index.js
+++ src/features/tasks/TasksPage/TasksList/index.js
@@ -2,10 +2,11 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { List, Item, Content, TaskButton } from "./styled";
 import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
+import searchQueryParamName from "../searchQueryParamName";
 
 const TasksList = () => {
   const location = useLocation();
-  const query = (new URLSearchParams(location.search)).get("szukaj");
+  const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
 
   const tasks = useSelector(state => selectTasksByQuery(state, query));
   const hideDone = useSelector(selectHideDone);
