d0df6b813a16cf238dbc3eb5224e0c57f4eee45d
Rafał Olewicz <namerafal75@gmail.com>
2024-11-30 20:27:37 +0100

    Start adding task search engine

---
+++ src/features/tasks/TasksPage/TasksList/index.js
@@ -0,0 +1,44 @@
+import { Link, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { List, Item, Content, TaskButton } from "./styled";
+import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery } from "../../tasksSlice";
+
+const TasksList = () => {
+  const location = useLocation();
+  const query = (new URLSearchParams(location.search)).get("szukaj");
+
+  const tasks = useSelector(state => selectTasksByQuery(state, query));
+  const hideDone = useSelector(selectHideDone);
+
+  const dispatch = useDispatch();
+
+  return (
+    <List>
+      {tasks.map(task => (
+        <Item
+          key={task.id}
+          hidden={task.done && hideDone}
+        >
+          <TaskButton
+            $toggleDone
+            onClick={() => dispatch(toggleTaskDone(task.id))}
+          >
+            {task.done ? "✔" : " "}
+          </TaskButton>
+          <Content $done={task.done}>
+            <Link to={`/zadania/${task.id}`}>{task.content}</Link>
+          </Content>
+          <TaskButton
+            $remove
+            onClick={() => dispatch(removeTask(task.id))}
+          >
+            🗑
+          </TaskButton>
+        </Item>
+      ))}
+    </List>
+  )
+};
+
+
+export default TasksList;
