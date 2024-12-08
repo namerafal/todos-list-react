dbd0c57beb18676bbb7dd82bec9a67b8fe66bd98
Rafał Olewicz <namerafal75@gmail.com>
2024-12-08 12:01:48 +0100

    Removed paragraph tag

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -11,21 +11,21 @@ const Author = () => (
             title="Rafał Olewicz"
 
             body={
-                <>                   
-                    <p>
-                        <b>Od wielu lat pasjonuję się elektroniką</b>, która stała się moim hobby. <br/>
-                        Interesuję się także technologią, fotografią oraz muzyką; jestem samoukiem w grze na perkusji,
-                        która dodaje mi energii i pozytywnych wrażeń, obecnie jest to moje ulubione hobby. <br/>
-                        Choć obecnie nie mam okazji kontynuować swoich działań w dziedzinie elektroniki,
-                        programowanie wciąż fascynuje mnie od momentu, gdy po raz pierwszy sięgnąłem po <i>Arduino</i>.
+                <>
 
-                        <h3>Dlaczego programowanie?</h3>
+                    <b>Od wielu lat pasjonuję się elektroniką</b>, która stała się moim hobby. <br />
+                    Interesuję się także technologią, fotografią oraz muzyką; jestem samoukiem w grze na perkusji,
+                    która dodaje mi energii i pozytywnych wrażeń, obecnie jest to moje ulubione hobby. <br />
+                    Choć obecnie nie mam okazji kontynuować swoich działań w dziedzinie elektroniki,
+                    programowanie wciąż fascynuje mnie od momentu, gdy po raz pierwszy sięgnąłem po <i>Arduino</i>.
 
-                        Programowanie stało się moim sposobem na spędzanie czasu. Kiedy siadam do komputera, <br/>
-                        czas mija niezauważalnie, co potwierdza moją pasję do tej dziedziny.<br/>
-                        Obecnie korzystam z kursu w szkole <b>YouCode</b>, co daje mi <em>możliwość rozwijania umiejętności</em><br/> 
-                        i mam nadzieję na <em>zmianę swojego stylu życia oraz dalszy rozwój</em> w obszarze technologii.
-                    </p>
+                    <h3>Dlaczego programowanie?</h3>
+
+                    Programowanie stało się moim sposobem na spędzanie czasu. Kiedy siadam do komputera, <br />
+                    czas mija niezauważalnie, co potwierdza moją pasję do tej dziedziny.<br />
+                    Obecnie korzystam z kursu w szkole <b>YouCode</b>, co daje mi <em>możliwość rozwijania umiejętności</em><br />
+                    i mam nadzieję na <em>zmianę swojego stylu życia oraz dalszy rozwój</em> w obszarze technologii.
+
                 </>
             }
         />
